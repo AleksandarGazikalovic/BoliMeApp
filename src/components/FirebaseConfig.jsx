056117447e654f9b6b5b46582d8bcc1196941375
AleksandarGazikalovic <aleksandargazikalovic@gmail.com>
2024-06-27 22:20:36 +0200

    Added firebase config and basic auth

---
+++ src/components/FirebaseConfig.jsx
@@ -0,0 +1,41 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDEo7qo-x4Z6GFN9ljtR_QlH5F_Ay9QQkw",
+  authDomain: "bolime-app.firebaseapp.com",
+  projectId: "bolime-app",
+  storageBucket: "bolime-app.appspot.com",
+  messagingSenderId: "661514278322",
+  appId: "1:661514278322:web:95f2f663ce8c432abf00ab",
+  measurementId: "G-T2N967M3FL",
+};
+
+export const firebase = initializeApp(firebaseConfig);
+export const auth = getAuth(firebase);
+
+export const login = async (email, password) => {
+  await signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+};
+
+export const register = async (email, password) => {
+  await createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+};
