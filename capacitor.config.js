620c2f3b3a6e98fd8265ca5a87d6306c7a2b9e5e
AleksandarGazikalovic <aleksandargazikalovic@gmail.com>
2024-06-27 13:14:39 +0200

    Switched from ts to js

---
+++ capacitor.config.js
@@ -0,0 +1,7 @@
+const config = {
+  appId: "io.ionic.starter",
+  appName: "BoliMeApp",
+  webDir: "dist",
+};
+
+module.exports = config;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
 
-const Home: React.FC = () => {
+const Home = () => {
   return (
     <IonPage>
       <IonHeader>
