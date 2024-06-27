20968d3d6345a6219a1d3620e29caadf92900c3b
AleksandarGazikalovic <aleksandargazikalovic@gmail.com>
2024-06-27 23:08:58 +0200

    Fixed input fields position

--- src/pages/Registracija.jsx
+++ src/pages/Registracija.jsx
@@ -4,41 +4,81 @@ import {
   IonTitle,
   IonButton,
   IonIcon,
-  IonInput, 
-  IonInputPasswordToggle
-
-
+  IonInput,
+  IonInputPasswordToggle,
 } from "@ionic/react";
-import { personCircleOutline } from 'ionicons/icons'; // Import the specific icon
+import { personCircleOutline } from "ionicons/icons"; // Import the specific icon
 import "./Registracija.css";
 
 const Registracija = () => {
-  
-
   return (
     <IonPage>
-      <IonContent fullscreen className="ion-padding " color="light" >
-      <IonTitle class="ion-text-center ion-text-lg reg-title" >Registracija</IonTitle>
-      <br/>
-        
-        <div className="flex flex-col justify-center">
-        <form action="" className=" ion-align-items-center">
-        <IonInput class="form-ele  ion-margin" label="Ime" type="text" placeholder="Marko" labelPlacement="floating" fill="outline"></IonInput>
-        <IonInput class="form-ele  ion-margin" label="Prezime" type="text" placeholder="Marković" labelPlacement="floating" fill="outline"></IonInput>
-        <IonInput class="form-ele  ion-margin" label="Datum rođenja" type="date" placeholder="dd/mm/gggg" labelPlacement="floating" fill="outline"></IonInput>
-        <IonInput class="form-ele  ion-margin" label="Email" type="email" placeholder="email@gmail.com" labelPlacement="floating" fill="outline"></IonInput>
-        <IonInput class="form-ele  ion-margin" label="Lozinka" type="password" placeholder="Lozinka" labelPlacement="floating" fill="outline" >
+      <IonContent fullscreen className="ion-padding " color="light">
+        <IonTitle className="ion-text-center ion-text-lg reg-title">
+          Registracija
+        </IonTitle>
+        <br />
+
+        <form action="" className="ion-ion-justify-content-center">
+          <IonInput
+            className="form-ele  ion-margin-bottom"
+            label="Ime"
+            type="text"
+            placeholder="Marko"
+            labelPlacement="floating"
+            fill="outline"
+          ></IonInput>
+          <IonInput
+            className="form-ele  ion-margin-bottom"
+            label="Prezime"
+            type="text"
+            placeholder="Marković"
+            labelPlacement="floating"
+            fill="outline"
+          ></IonInput>
+          <IonInput
+            className="form-ele  ion-margin-bottom"
+            label="Datum rođenja"
+            type="date"
+            placeholder="dd/mm/gggg"
+            labelPlacement="floating"
+            fill="outline"
+          ></IonInput>
+          <IonInput
+            className="form-ele  ion-margin-bottom"
+            label="Email"
+            type="email"
+            placeholder="email@gmail.com"
+            labelPlacement="floating"
+            fill="outline"
+          ></IonInput>
+          <IonInput
+            className="form-ele  ion-margin-bottom"
+            label="Lozinka"
+            type="password"
+            placeholder="Lozinka"
+            labelPlacement="floating"
+            fill="outline"
+          >
             <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
-            </IonInput>
-            <IonInput class="form-ele  ion-margin" label="Potvrdi lozinku" type="password" placeholder="Lozinke moraju da se poklapaju" labelPlacement="floating" fill="outline" >
-            
-            </IonInput>
-            <IonButton type="submit" expand="block" className="ion-margin ion-justify-center ion-padding">Registruj me
-              <IonIcon icon={personCircleOutline} slot="end"></IonIcon>
-            </IonButton>
-      </form>
-        </div>
- 
+          </IonInput>
+          <IonInput
+            className="form-ele  ion-margin-bottom"
+            label="Potvrdi lozinku"
+            type="password"
+            placeholder="Lozinke moraju da se poklapaju"
+            labelPlacement="floating"
+            fill="outline"
+          ></IonInput>
+          <IonButton
+            type="submit"
+            expand="block"
+            className="ion-margin-top ion-justify-center ion-padding"
+          >
+            Registruj me
+            <IonIcon icon={personCircleOutline} slot="end"></IonIcon>
+          </IonButton>
+        </form>
       </IonContent>
     </IonPage>
   );
