cf19ce6602b159b415a6fb5fd6170cbcf2518aed
AleksandarGazikalovic <aleksandargazikalovic@gmail.com>
2024-06-29 18:28:23 +0200

    Basic fetching of user profiles

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -7,68 +7,78 @@ import {
   IonHeader,
   IonTitle,
   IonIcon,
-
+  IonList,
+  IonItem,
+  IonLabel,
 } from "@ionic/react";
 import logo from "../assets/logo.png";
 import "./LandingPage.css";
 import { addOutline } from "ionicons/icons";
 import { auth } from "../components/FirebaseConfig";
-
+import { profileService } from "./../services";
 
 const LandingPage = () => {
-  const [userId, setUserId] = useState(null);
+  const [profiles, setProfiles] = useState([]);
 
-    useEffect(() => {
-      const unsubscribe = auth.onAuthStateChanged(user => {
-        if (user) {
-          setUserId(user.uid); // Set userId state when user is authenticated
-        } else {
-          setUserId(null); // Handle state when user is not authenticated
-        }
-      });
-      return () => unsubscribe();
-    }, []);
-
-    console.log("Current userId:", userId)
- 
-
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        profileService.getProfilesByUserId(user.uid).then((data) => {
+          setProfiles(data);
+        });
+      } else {
+        setUserId(null);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar className="ion-toolbar">
-            <div className="header">
-            
-          <div className="logo-side">
-            <img src={logo} alt="Logo" className="logo" />
-            <h1 className="logo-h1">Boli me</h1>
-          </div>
-       
+          <div className="header">
+            <div className="logo-side">
+              <img src={logo} alt="Logo" className="logo" />
+              <h1 className="logo-h1">Boli me</h1>
+            </div>
+
             <div className="button-side">
-            <IonButton
-              size="medium"
-              fill="solid"
-              color="primary"
-            >
-              Izmeni profile
-            </IonButton>
-          </div>
+              <IonButton size="medium" fill="solid" color="primary">
+                Izmeni profile
+              </IonButton>
+            </div>
           </div>
         </IonToolbar>
       </IonHeader>
 
       <IonContent fullscreen color="light">
-        <IonTitle className="ion-padding ion-text-center ioTitle" color={"primary"} >Koga boli?</IonTitle>
-        <br/>
-        <br/>
+        <IonTitle
+          className="ion-padding ion-text-center ioTitle"
+          color={"primary"}
+        >
+          Koga boli?
+        </IonTitle>
+        <br />
+        <br />
+        <IonList>
+          {profiles.map((profile) => (
+            <IonItem key={profile.id}>
+              <IonLabel>
+                <h2>
+                  {profile.name} {profile.surname}
+                </h2>
+              </IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
         <div className="avatar-conatiner">
-            <div className="add-profile">
-            <IonButton size="large" routerLink="/create-profile" >
-                <IonIcon slot="icon-only" md={addOutline}></IonIcon>
+          <div className="add-profile">
+            <IonButton size="large" routerLink="/create-profile">
+              <IonIcon slot="icon-only" md={addOutline}></IonIcon>
             </IonButton>
             <h3 className="dodaj-h3">Dodaj novi profil</h3>
-            </div>
-            
+          </div>
         </div>
       </IonContent>
     </IonPage>
