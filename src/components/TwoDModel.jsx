22f013ced7bb7d6d59442e4cc57c8df6c49877a5
AleksandarGazikalovic <aleksandargazikalovic@gmail.com>
2024-06-30 11:40:51 +0200

    Added spinning to rotate icon

--- src/components/TwoDModel.jsx
+++ src/components/TwoDModel.jsx
@@ -1,14 +1,31 @@
+import React, { useState } from "react";
 import { syncOutline } from "ionicons/icons";
-import { IonButton, IonIcon } from "@ionic/react";
-import { useState } from "react";
+import { IonIcon } from "@ionic/react";
+import "./TwoDModel.css"; // Make sure to import your CSS file
 
-const TwoDModel = ({}) => {
-  const handleRotation = () => {};
+const TwoDModel = () => {
+  const [rotation, setRotation] = useState(0);
+  const [isPrimary, setIsPrimary] = useState(false);
+
+  const handleRotation = () => {
+    if (isPrimary) return;
+    setRotation((prevRotation) => prevRotation + 180);
+    setIsPrimary(true);
+    setTimeout(() => {
+      setIsPrimary(false);
+    }, 500); // Duration of the color change
+  };
+
   return (
     <div className="ion-text-center ion-padding">
-      <h1>Selektujte mesto koje vas boli</h1>
-
-      <IonIcon icon={syncOutline} onClick={handleRotation} size="large" />
+      <h1>Izaberite mesto koje vas boli</h1>
+      <IonIcon
+        icon={syncOutline}
+        onClick={handleRotation}
+        size="large"
+        className={`spin ${isPrimary ? "primary-color" : ""}`}
+        style={{ transform: `rotate(${rotation}deg)` }}
+      />
     </div>
   );
 };
